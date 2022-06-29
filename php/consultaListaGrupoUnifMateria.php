cf173d95d9b7696e4b7879519950e418a50760d6
Ericka Cardenas <cardenas.ericka1697@gmail.com>
2022-06-29 17:19:19 -0400

    lista completa de horarios

--- php/consultaListaGrupoUnifMateria.php
+++ php/consultaListaGrupoUnifMateria.php
@@ -12,13 +12,13 @@ d.`codigoSis` AS CodigoDocente,
 CONCAT(d.`nombre`,' ',d.`apellido`) AS NombreDocente,
 g.`estudiantes`,
 CONCAT(h.`dia`,' ',h.`horaInicio`,' - ',h.`horaFin`) AS Horario 
-FROM 
-`Grupo` AS g, 
-`Materia` AS m,
-`Docente` AS d,
-`Horario` AS h 
-WHERE 
-g.`codMateria` = m.`codMateria` AND g.`codDocente` = d.`codigoSis` AND h.`codGrupo` = g.`codGrupo` AND h.`codMateria` = g.`codMateria`;";
+FROM
+`Grupo` AS g 
+LEFT JOIN 
+`Materia` AS m 
+ON m.`codMateria` = g.`codMateria`
+LEFT JOIN `Docente` AS d ON d.`codigoSis` = g.`codDocente`
+LEFT JOIN `Horario` AS h ON h.`codGrupo` = g.`codGrupo` ORDER BY g.`codGrupo`,g.`codMateria`;";
 $respuesta= mysqli_query($conexion,$consulta);
 $res = mysqli_fetch_all( $respuesta, $resulttype = MYSQLI_ASSOC);
 
